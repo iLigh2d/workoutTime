dcc0a20bcd26f4518bb58776f57c55580ba122e4
iLigh2d <111642998+iLigh2d@users.noreply.github.com>
2022-11-02 22:38:39 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,40 +1,27 @@
 const burgerBtn = document.querySelector('#nav-icon3')
 const headerBody = document.querySelector('.header__body')
+const header = document.querySelector('.header__items')
 
 burgerBtn.addEventListener('click', showBurger)
 
 function showBurger() {
-    headerBody.classList.toggle('active')
-    burgerBtn.classList.toggle('active')
+    headerBody.classList.toggle('_active')
+    burgerBtn.classList.toggle('_active')
     document.body.classList.toggle('_lock')
 }
 
-//=======<SLIDER>=========
-const swiper = new Swiper(".mySwiper", {
-    effect: "coverflow",
-    grabCursor: true,
-    centeredSlides: true,
-    slidesPerView: 1,
-    initialSlide: 1,
-    coverflowEffect: {
-      rotate: 50,
-      stretch: 0,
-      depth: 100,
-      modifier: 1,
-      slideShadows: true,
-    },
-    breakpoints: {
-        768: {
-          slidesPerView: 3,
-          initialSlide: 1
-        },
-      },
-    pagination: {
-      el: ".swiper-pagination",
-    },
-  });
+//============HIDE HEADER======
+const intro = document.querySelector('.header')
 
-//===============
+window.addEventListener('scroll', function() {
+  if(scrollY > intro.offsetHeight) {
+    header.classList.add('header__fixed')
+  } else {
+    header.classList.remove('header__fixed')
+  } 
+})
+
+//===============SCROLL===========
 const menuLinks = document.querySelectorAll(".menu__link[data-goto]");
 
 if(menuLinks.length > 0){
@@ -44,27 +31,86 @@ if(menuLinks.length > 0){
 }
 function onMenuLink(e){
   const menuLink = e.target;
-  headerBody.classList.remove('active')
-  burgerBtn.classList.remove('active')
+  headerBody.classList.remove('_active')
+  burgerBtn.classList.remove('_active')
   if(menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)){
     const gotoBlock = document.querySelector(menuLink.dataset.goto);
     const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('.header__items').offsetHeight -50
-
+    
     window.scrollTo({
       top: gotoBlockValue,
       behavior: 'smooth',
     });
     e.preventDefault();
+
+    document.body.classList.remove('_lock')
   }
 }
-//============
-const intro = document.querySelector('.header')
-const header = document.querySelector('.header__items')
 
-window.addEventListener('scroll', function() {
-  if(scrollY > intro.offsetHeight) {
-    header.classList.add('header__fixed')
-  } else {
-    header.classList.remove('header__fixed')
-  } 
-})
+//====================SLIDER=======================
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  slidesPerGroup: 1,
+  speed: 1000,
+  loop: true,
+  breakpoints: {
+        1100: {
+          slidesPerView: 3,
+          slidesPerGroup: 3,
+        },
+        768: {
+          slidesPerView: 2,
+          slidesPerGroup: 2,
+        },
+      },
+        
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+//========Animation blocks==========
+let animItems = document.querySelectorAll('._anim');
+
+if(animItems.length > 0) {
+	window.addEventListener("scroll", animOnScroll);
+	function animOnScroll(start) {
+		for (let index = 0; index < animItems.length; index++) {
+			const animItem = animItems[index];
+			const animItemHeight = animItem.offsetHeight;
+			const animItemOffset = offset(animItem).top;
+			const animStart = 5;
+
+			let animItemPoint = window.innerHeight - animItemHeight / animStart;
+			if(animItemHeight > window.innerHeight){
+				animItemPoint = window.innerHeight - window.innerHeight / animStart;
+
+			}
+
+			if((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)){
+				animItem.classList.add("_hide");
+			} else{
+				if(!animItem.classList.contains('anim-no-height')){
+				animItem.classList.remove("_hide");
+			}
+		}
+	}
+}
+	function offset(el){
+		const rect = el.getBoundingClientRect(),
+		scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+		scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+	return{top: rect.top + scrollTop, left: rect.left + scrollLeft}
+	}
+
+	setTimeout(() =>{
+		animOnScroll();
+	}, 300);
+	
+}	
